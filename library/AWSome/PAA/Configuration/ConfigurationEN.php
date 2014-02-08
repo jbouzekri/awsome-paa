261a139c304f3d6abcf1f25c3b92ce641ccb8c65
jbouzekri <jonathan.bouzekri@gmail.com>
2014-02-08 19:21:00 +0100

    init EN configuration

---
+++ library/AWSome/PAA/Configuration/ConfigurationEN.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright 2014 Jonathan Bouzekri. All rights reserved.
+ *
+ * @copyright Copyright 2014 Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
+ * @license https://github.com/jbouzekri/awsome-pma/blob/master/LICENSE
+ * @link https://github.com/jbouzekri/awsome-pma
+ */
+
+/**
+ * @namespace
+ */
+namespace AWSome\PAA\Configuration;
+
+/**
+ * Description of ConfigurationEN
+ *
+ * @author jobou
+ */
+class ConfigurationEN extends Configuration
+{
+    /**
+     * The locale for this configuration
+     *
+     * @var string
+     */
+    protected $locale = "EN";
+
+    /**
+     * Hostname
+     *
+     * @var string
+     */
+    protected $hostname = "webservices.amazon.com";
+}
--- library/AWSome/PAA/Configuration/ConfigurationFactory.php
+++ library/AWSome/PAA/Configuration/ConfigurationFactory.php
@@ -18,20 +18,21 @@ use AWSome\PAA\Exception\ConfigurationException;
 /**
  * @namespace
  */
-class ConfigurationFactory 
+class ConfigurationFactory
 {
     /**
      * An array of mapping between locale and its configuration class
-     * 
+     *
      * @var array
      */
     private static $availableConfiguration = array(
-        'FR' => 'AWSome\\PAA\\Configuration\\Configuration'
+        'FR' => 'AWSome\\PAA\\Configuration\\Configuration',
+        'EN' => 'AWSome\\PAA\\Configuration\\ConfigurationEN',
     );
-    
+
     /**
      * Get a configuration instance according to your locale
-     * 
+     *
      * @param string $locale Query locale
      */
     public function get($locale)
@@ -39,20 +40,20 @@ class ConfigurationFactory
         if (!array_key_exists($locale, static::$availableConfiguration)) {
             throw new ConfigurationException('No configuration specified for locale '.$locale);
         }
-        
+
         $configuration = new static::$availableConfiguration[$locale];
         if (!$configuration instanceof Configuration) {
             throw new ConfigurationException(
                 'Configuration ' . get_class($configuration) . ' is not an instance of Configuration'
             );
         }
-        
+
         return $configuration;
     }
-    
+
     /**
      * Register new configurations in the factory
-     * 
+     *
      * @param array $configurations an array of configurations
      *      the key must match an available locale and the value must match a configuration class name
      */
